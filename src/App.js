bca6cb821dee69525578d31d7be2f0fdd65c13a3
tpshaheem5 <shaheemtp5@gmail.com>
2023-06-09 11:53:53 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.css';
+
+
 
 function App() {
+  const [post, setPost] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((post) => setPost(post.splice(0, 10)));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="bg-info">Fetch API Data</h1>
+      {post.map((post) => (
+        <div key ={post.id} className="bg-danger " >
+        <h1>{post.title}</h1>
+        </div>
+      ))}
     </div>
   );
 }
